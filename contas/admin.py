c7641ae08e97f4555c5d10e9a545945997b79e48
Chris N <chriss.noga@gmail.com>
2019-01-05 20:34:05 -0200

    Added model Transacao - migrate

--- contas/admin.py
+++ contas/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Categoria
+from .models import Transacao
 
 admin.site.register(Categoria)
+admin.site.register(Transacao)
