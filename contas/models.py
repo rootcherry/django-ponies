ab7f7fa76863002d130012a9d37a036fa77b63e7
Chris N <chriss.noga@gmail.com>
2019-01-05 20:07:52 -0200

    Added model Categoria - migrate

--- contas/models.py
+++ contas/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+	nome = models.CharField(max_length=100)
+	data_criacao = models.DateTimeField(auto_now_add=True)
